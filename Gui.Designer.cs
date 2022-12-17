6752df230f5dc212d0529c0b27b71807d822c399
Harm <git@harm.io>
2022-12-17 16:41:20 +0100

    V1.0 files from 2018

---
+++ Gui.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace MKDX_Unlocked_Profile
+{
+    partial class Gui
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Gui));
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.link = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(159, 6);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(100, 20);
+            this.tbName.TabIndex = 1;
+            this.tbName.Text = "Unlocked";
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(12, 29);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(249, 117);
+            this.lblInstructions.TabIndex = 4;
+            this.lblInstructions.Text = resources.GetString("lblInstructions.Text");
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(12, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(75, 13);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Enter name:";
+            // 
+            // link
+            // 
+            this.link.AutoSize = true;
+            this.link.BackColor = System.Drawing.Color.Transparent;
+            this.link.Location = new System.Drawing.Point(52, 119);
+            this.link.Name = "link";
+            this.link.Size = new System.Drawing.Size(32, 13);
+            this.link.TabIndex = 6;
+            this.link.TabStop = true;
+            this.link.Text = "Harm";
+            this.link.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.link_LinkClicked);
+            // 
+            // Gui
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 164);
+            this.Controls.Add(this.link);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.tbName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Gui";
+            this.Text = "MKDX Unlocked Profile v1.0";
+            this.Load += new System.EventHandler(this.Gui_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.LinkLabel link;
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # MKDX-Unlocked-Profile
+
+100% unlocked offline savegame for Mario Kart Arcade GP DX.
+Use with TeknoParrot.
+
+## Version History
+
+### 1.0 (2018)
+
+* Original version for MKDX 1.10
+* Is missing QR unlockable karts
